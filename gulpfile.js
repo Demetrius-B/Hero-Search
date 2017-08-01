a24bc3f3875e09053a59fdb55835114fb8edd2ec
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-01 14:08:29 -0400

    Added BrowerSync to gulp. Finished cover page

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const gulp = require("gulp")
-const sass = require('gulp-ruby-sass');
+const sass = require('gulp-ruby-sass')
+const browserSync = require('browser-sync').create()
 
 gulp.task('sass', function() {
   return sass('./sass/*.sass')
       .on('error', sass.logError)
       .pipe(gulp.dest('./css'))
+      .pipe(browserSync.reload({
+        stream:true
+      }))
 })
 
-gulp.task('watch', function() {
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
   gulp.watch('./sass/*.sass', ['sass'])
 })
